1f1361717751df809179b709cceb025fa11a6433
Mohitbyte29 <mohitupadhyay1025@gmail.com>
2025-08-31 17:27:38 +0530

    update code

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     let slider = document.querySelector('.star-content');
         let rightBtn = document.querySelector('#star-right');
+        let leftBtn = document.querySelector('#')
     
         rightBtn.addEventListener('click', () => {
             let scrollAmount = slider.querySelector('.star-box').offsetWidth + 15;
